d50353eb3d328593db2d0c31ca5bb3b3eb3a5fd1
Randall Wade <rwade727@gmail.com>
2021-01-05 17:12:49 +0000

    learned how to copy files in python

---
+++ advlist/animal-list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+animals = ["Fox"," Fly"," Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+print(animals)
+
+challenge= [1, 2, 3, ["a", "b", "c", [1, 4, 7, "yeet"]]]
+
+print("I think we all can agree that The Lion King is improved by a good" ,challenge[3][3][3]+".")
+
---
+++ icecream.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+icecream= ["flavors", "salty"]
+icecream.append(99)
+txt = input("What is your name? ")
+print(str(icecream[2]) , icecream[0] + ", and" , txt , "chooses to be " , icecream[1])
